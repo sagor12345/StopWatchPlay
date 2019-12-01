771c403e6e7d206675c41535aea35c0ea66c10c1
MD. SHAMIM HOSSAIN <mdshamimh529@gmail.com>
2019-12-01 11:39:32 +0600

    Add Reset Button

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -13,34 +13,55 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button start,finish;
+    Button start, finish ,reset;
     Chronometer chronometer;
     Animation animation;
     ImageView imageView;
+    long timeWhenStopped = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        start =findViewById(R.id.buttonStart);
+        start = findViewById(R.id.buttonStart);
         finish = findViewById(R.id.buttonFinish);
+        reset = findViewById(R.id.reset);
         chronometer = findViewById(R.id.time);
-        imageView =findViewById(R.id.image);
-        animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.rotation);
+        imageView = findViewById(R.id.image);
+
+        animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.rotation);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 imageView.startAnimation(animation);
                 chronometer.setFormat("%s");
-               chronometer.setBase(SystemClock.elapsedRealtime());
+                //chronometer.setBase(SystemClock.elapsedRealtime());
                 chronometer.start();
                 start.setVisibility(View.INVISIBLE);
                 finish.setVisibility(View.VISIBLE);
+                reset.setVisibility(View.INVISIBLE);
+                chronometer.setBase(SystemClock.elapsedRealtime() + timeWhenStopped);
+
             }
         });
         finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.stop();
+                //chronometer.setBase(SystemClock.elapsedRealtime());
+                animation.cancel();
+                imageView.setAnimation(animation);
+                start.setVisibility(View.VISIBLE);
+                finish.setVisibility(View.INVISIBLE);
+                reset.setVisibility(View.VISIBLE);
+                timeWhenStopped = chronometer.getBase() - SystemClock.elapsedRealtime();
+
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 chronometer.stop();
@@ -49,9 +70,10 @@ public class MainActivity extends AppCompatActivity {
                 imageView.setAnimation(animation);
                 start.setVisibility(View.VISIBLE);
                 finish.setVisibility(View.INVISIBLE);
+                reset.setVisibility(View.VISIBLE);
+                //timeWhenStopped = chronometer.getBase() - SystemClock.elapsedRealtime();
 
             }
         });
-
     }
 }
